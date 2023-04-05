62f2186415ba3ad684eab9dcac2867bfcfe56444
kalyandevops <avsrpavankalyan@gmail.com>
2023-04-05 22:01:14 +0530

    final_output_task

--- main.tf
+++ main.tf
@@ -3,8 +3,14 @@ resource "aws_instance" "terraform_vm2" {
     instance_type = "t2.micro"
     key_name = "terraform"
     security_groups = [ "ec2_securitygroup" ]
+    connection {
+      type = "ssh"
+      host = self.public_ip
+      user = "ubuntu"
+      private_key = ("~/.ssh/id_rsa")
+    }
     provisioner "local-exec" {
-        command = "echo $kalyan"
+        command = "echo KalyanKR >> Taskfile.txt"
       
     }
     tags = {
